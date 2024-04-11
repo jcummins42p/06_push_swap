6111ecba3372746580b83606450abb5be54e82f3
jcummins <jcummins42prague@gmail.com>
2024-04-11 17:13:56 +0200

    Reverting free_flag in main to int rather than bool to track down memory leaks

--- push_swap/srcs/main.c
+++ push_swap/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jcummins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 14:09:10 by jcummins          #+#    #+#             */
-/*   Updated: 2024/04/11 16:30:52 by jcummins         ###   ########.fr       */
+/*   Updated: 2024/04/11 17:13:23 by jcummins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,10 +61,10 @@ void	sort_options(t_stack **a)
 
 int	main(int argc, char **argv)
 {
-	bool	free_flag;
+	int		free_flag;
 	t_stack	*a;
 
-	free_flag = false;
+	free_flag = 0;
 	a = NULL;
 	if (argv == NULL)
 		return (1);
@@ -74,7 +74,7 @@ int	main(int argc, char **argv)
 		if (argv)
 		{
 			argv[0] = "placeholder";
-			free_flag = true;
+			free_flag = 1;
 		}
 	}
 	if (argv && list_init(&a, &argv[1]))
