9c999d1666631c43e0e42b6e9dcaf6dddec3ad64
jcummins <jcummins42prague@gmail.com>
2024-04-15 18:12:30 +0200

    Updating bonus header

--- include/bonus.h
+++ include/bonus.h
@@ -6,7 +6,7 @@
 /*   By: jcummins <jcummins@student.42prague.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 15:47:26 by jcummins          #+#    #+#             */
-/*   Updated: 2024/04/15 17:41:33 by jcummins         ###   ########.fr       */
+/*   Updated: 2024/04/15 18:11:49 by jcummins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,42 +37,42 @@ int				list_init(t_stack **a, char **argv);
 //		ft_itoa_pad.c
 char			*ft_itoa_pad(int n, int pad);
 
+//		ft_itoa.c
+char			*ft_itoa(int n);
+
 //		ft_printf.c
 int				ft_printf(const char *str, ...);
 
 //		ft_strncmp.c
 int				ft_strncmp(const char *s1, const char *s2, size_t n);
 
-//		ft_itoa.c
-char			*ft_itoa(int n);
-
 //		ft_split.c	-	To format input given as a single string into argv arr.
 char			**ft_split(char const *s, char c);
 
-//		visualise.c
+//		visualise_bonus.c
 void			draw_stacks(t_stack **a, t_stack **b);
 
-//		validation.c	-	input checks
+//		validation_bonus.c	-	input checks
 long int		ft_atol(char const *str);
 int				valid_range(long n);
 int				valid_uniq(t_stack **a, int n);
 int				valid_numb(char **argv);
 
-//		push.c
+//		push_bonus.c
 void			pa(t_stack **b, t_stack **a);
 void			pb(t_stack **a, t_stack **b);
 
-//		swap.c
+//		swap_bonus.c
 void			sa(t_stack **a);
 void			sb(t_stack **b);
 void			ss(t_stack **a, t_stack **b);
 
-//		rotate.c
+//		rotate_bonus.c
 void			ra(t_stack **a);
 void			rb(t_stack **b);
 void			rr(t_stack **a, t_stack **b);
 
-//		rev_rotate.c
+//		rev_rotate_bonus.c
 void			rra(t_stack **a);
 void			rrb(t_stack **b);
 void			rrr(t_stack **a, t_stack **b);
